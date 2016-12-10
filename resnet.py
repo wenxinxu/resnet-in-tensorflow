2bdc961709eef6bf89113926546e2160558d758d
Wenxin Xu <wenxin.xu2013@gmail.com>
2016-12-10 18:08:38 -0500

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -40,6 +40,11 @@ def create_variables(name, shape, initializer=tf.contrib.layers.xavier_initializ
 
 
 def output_layer(input_layer, num_labels):
+    '''
+    :param input_layer: 2D tensor
+    :param num_labels: int. How many output labels in total? (10 for cifar10 and 100 for cifar100)
+    :return: output layer Y = WX + B
+    '''
     input_dim = input_layer.get_shape().as_list()[-1]
     fc_w = create_variables(name='fc_weights', shape=[input_dim, num_labels], is_fc_layer=True,
                             initializer=tf.uniform_unit_scaling_initializer(factor=1.0))
@@ -50,6 +55,12 @@ def output_layer(input_layer, num_labels):
 
 
 def batch_normalization_layer(input_layer, dimension):
+    '''
+    Helper function to do batch normalziation
+    :param input_layer: 4D tensor
+    :param dimension: input_layer.get_shape().as_list()[-1]. The depth of the 4D tensor
+    :return: the 4D tensor after being normalized
+    '''
     mean, variance = tf.nn.moments(input_layer, axes=[0, 1, 2])
     beta = tf.get_variable('beta', dimension, tf.float32,
                                initializer=tf.constant_initializer(0.0, tf.float32))
@@ -61,9 +72,16 @@ def batch_normalization_layer(input_layer, dimension):
 
 
 def conv_bn_relu_layer(input_layer, filter_shape, stride):
+    '''
+    A helper function to conv, batch normalize and relu the input tensor sequentially
+    :param input_layer: 4D tensor
+    :param filter_shape: list. [filter_height, filter_width, filter_depth, filter_number]
+    :param stride: stride size for conv
+    :return: 4D tensor. Y = Relu(batch_normalize(conv(X)))
+    '''
+
     out_channel = filter_shape[-1]
     filter = create_variables(name='conv', shape=filter_shape)
-
 
     conv_layer = tf.nn.conv2d(input_layer, filter, strides=[1, stride, stride, 1], padding='SAME')
     bn_layer = batch_normalization_layer(conv_layer, out_channel)
@@ -73,6 +91,14 @@ def conv_bn_relu_layer(input_layer, filter_shape, stride):
 
 
 def bn_relu_conv_layer(input_layer, filter_shape, stride):
+    '''
+    A helper function to batch normalize, relu and conv the input layer sequentially
+    :param input_layer: 4D tensor
+    :param filter_shape: list. [filter_height, filter_width, filter_depth, filter_number]
+    :param stride: stride size for conv
+    :return: 4D tensor. Y = conv(Relu(batch_normalize(X)))
+    '''
+
     in_channel = input_layer.get_shape().as_list()[-1]
 
     bn_layer = batch_normalization_layer(input_layer, in_channel)
@@ -85,8 +111,16 @@ def bn_relu_conv_layer(input_layer, filter_shape, stride):
 
 
 def residual_block(input_layer, output_channel, first_block=False):
+    '''
+    Defines a residual block in ResNet
+    :param input_layer: 4D tensor
+    :param output_channel: int. return_tensor.get_shape().as_list()[-1] = output_channel
+    :param first_block: if this is the first residual block of the whole network
+    :return: 4D tensor.
+    '''
     input_channel = input_layer.get_shape().as_list()[-1]
 
+    # When it's time to "shrink" the image size, we use stride = 2
     if input_channel * 2 == output_channel:
         increase_dim = True
         stride = 2
@@ -96,6 +130,7 @@ def residual_block(input_layer, output_channel, first_block=False):
     else:
         raise ValueError('Output and input channel does not match in residual blocks!!!')
 
+    # The first conv layer of the first residual block does not need to be normalized and relu-ed.
     with tf.variable_scope('conv1_in_block'):
         if first_block:
             filter = create_variables(name='conv', shape=[3, 3, input_channel, output_channel])
@@ -106,6 +141,8 @@ def residual_block(input_layer, output_channel, first_block=False):
     with tf.variable_scope('conv2_in_block'):
         conv2 = bn_relu_conv_layer(conv1, [3, 3, output_channel, output_channel], 1)
 
+    # When the channels of input layer and conv2 does not match, we add zero pads to increase the
+    #  depth of input layers
     if increase_dim is True:
         pooled_input = tf.nn.avg_pool(input_layer, ksize=[1, 2, 2, 1],
                                       strides=[1, 2, 2, 1], padding='VALID')
@@ -120,8 +157,14 @@ def residual_block(input_layer, output_channel, first_block=False):
 
 def inference(input_tensor_batch, n, reuse):
     '''
-    total layers = 1 + 2n + 2n + 2n +1 = 6n + 2
+    The main function that defines the ResNet. total layers = 1 + 2n + 2n + 2n +1 = 6n + 2
+    :param input_tensor_batch: 4D tensor
+    :param n: num_residual_blocks
+    :param reuse: To build train graph, reuse=False. To build validation graph and share weights
+    with train graph, resue=True
+    :return: last layer in the network. Not softmax-ed
     '''
+
     layers = []
     with tf.variable_scope('conv0', reuse=reuse):
         conv0 = conv_bn_relu_layer(input_tensor_batch, [3, 3, 3, 16], 1)
@@ -161,11 +204,11 @@ def inference(input_tensor_batch, n, reuse):
 
     return layers[-1]
 
+
 def test_graph(train_dir='logs'):
     '''
     Run this function to look at the graph structure on tensorboard. A fast way!
     :param train_dir:
-    :return:
     '''
     input_tensor = tf.constant(np.ones([128, 32, 32, 3]), dtype=tf.float32)
     result = inference(input_tensor, 2, reuse=False)
